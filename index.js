ddfedcfc76165ec646eadd64db043c2d0ccaba73
raeesa99 <raeesapatel99@hotmail.com>
2022-12-06 11:59:31 +0000

    added handlebars

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express = require('express'); 
+const path = require('path');
+const {engine} = require('express-handlebars');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('app listening on http://localhost:3000');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//when app (server) gets a GET request to the /, do this:
+
+app.get ('/', (req, res) =>{
+    res.render('home');
 });
+
+app.get ('/about', (req, res) =>{
+    res.render('about');
+});
+
+app.get ('/signup', (req, res) =>{
+    res.render('signup');
+});
+
+app.get ('/login', (req, res) =>{
+    res.render('login');
+});
+
+app.listen(3000, () => {
+    console.log('app listening on http:localhost:3000');
+})
